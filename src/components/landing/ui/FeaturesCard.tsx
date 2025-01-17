327cf1d0d1e6d146b08b99ae8d4a9afb819bc334
Onyiriuka Justin <richardjustin500@gmail.com>
2025-01-17 21:29:54 +0100

    updates to features section

---
+++ src/components/landing/ui/FeaturesCard.tsx
@@ -0,0 +1,17 @@
+import { type FeaturesTypes } from "../constants/Features";
+
+function FeaturesCard({ features }: { features: FeaturesTypes }) {
+  return (
+    <article className="flex flex-col items-center max-w-[20rem] border border-white/20 rounded-lg gap-2 p-5 bg-[#0D0D0D]">
+      <div className="size-14 bg-white rounded-xl flex items-center justify-center">
+        {features.icon}
+      </div>
+      <h1 className="text-lg font-semibold">{features.title}</h1>
+      <p className="text-sm text-center text-pretty font-light">
+        {features.description}
+      </p>
+    </article>
+  );
+}
+
+export default FeaturesCard;
