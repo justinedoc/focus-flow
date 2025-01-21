2a8e8f8d71e6209b07259703d6cd296efc1ec005
Onyiriuka Justin <richardjustin500@gmail.com>
2025-01-21 17:11:28 +0100

    routes added

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
+import { Route, Routes } from "react-router";
 import Signup from "./components/auth/pages/Signup";
 import LandingPage from "./components/landing/LandingPage";
 
 function App() {
   // return <LandingPage />;
-  return <Signup />
+  return (
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/signup" element={<Signup />} />
+    </Routes>
+  );
 }
 
 export default App;
