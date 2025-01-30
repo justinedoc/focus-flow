9e283047ee2e79608eccfd939f9f999a3330cd75
Onyiriuka Justin <richardjustin500@gmail.com>
2025-01-30 13:07:50 +0100

    dashboard route added #2

---
+++ src/components/dashboard/_components/Dashboard.tsx
@@ -0,0 +1,13 @@
+import Navbar from "../ui/Navbar";
+
+function Dashboard() {
+  return (
+    <div className="mt-44 mx-auto">
+      <Navbar />
+
+      <h1 className="text-3xl text-center">Under construction</h1>
+    </div>
+  );
+}
+
+export default Dashboard;
