71b50e7bbc6bf49344a71d7008b9c6cc2bd7f60d
Onyiriuka Justin <richardjustin500@gmail.com>
2025-01-30 13:07:40 +0100

    dashboard route added

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router";
 import Signup from "./components/auth/pages/Signup";
 import LandingPage from "./components/landing/LandingPage";
 import Login from "./components/auth/pages/Login";
+import Dashboard from "./components/dashboard/_components/Dashboard";
 
 function App() {
   // return <LandingPage />;
@@ -10,6 +11,7 @@ function App() {
       <Route path="/" element={<LandingPage />} />
       <Route path="/signup" element={<Signup />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/dashboard" element={<Dashboard />} />
       <Route path="*" element={<h1>Not found</h1>} />
     </Routes>
   );
